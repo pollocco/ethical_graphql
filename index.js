21706dbccdeb20ce7a282e1e574be4a89d76a00a
pollocco <58239644+pollocco@users.noreply.github.com>
2020-11-01 13:33:36 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -13,9 +13,38 @@ app.use(cors())
 const schema = buildASTSchema(gql `
     type Query {
         hello: String
+        getIngredients: [Ingredient]
         getUsers: [User]
         getCrops: [Crop]
+        getRecipes: [Recipe]
         findCrop(filter: String): [Crop]
+    }
+
+    type Recipe {
+        id: ID
+        name: String
+        ingredients: [RecipeIngredient]
+    }
+
+    type Ingredient{
+        name: String
+    }
+
+    input IngredientInput{
+        name: String
+    }
+
+    type RecipeIngredient{
+        recipe: Recipe
+        ingredient: Ingredient
+        amount: Float
+        unit: String
+    }
+
+    input RecipeIngredientInput{
+        ingredient: IngredientInput
+        amount: Float
+        unit: String
     }
 
     type Crop {
@@ -34,11 +63,16 @@ const schema = buildASTSchema(gql `
     
     type Mutation {
         newUser(name: String, email: String, id:Int): User
+        newIngredient(name:String): Ingredient
+        newRecipe(id: ID, name: String, ingredients: [RecipeIngredientInput]): Recipe
     }
 `)
 
 const USERS = new Map()
 const CROPS = new Map()
+const INGREDIENTS = new Map()
+const RECIPES = new Map()
+const RECIPEINGREDIENTS = new Map()
 
 
 class User {
@@ -46,6 +80,18 @@ class User {
 }
 
 class Crop{
+    constructor(data){Object.assign(this, data)}
+}
+
+class Ingredient{
+    constructor(data){Object.assign(this, data)}
+}
+
+class Recipe{
+    constructor(data){Object.assign(this, data)}
+}
+
+class RecipeIngredient{
     constructor(data){Object.assign(this, data)}
 }
 
@@ -61,12 +107,37 @@ crops.forEach(crop => {
 
 const rootValue = {
     hello: () => 'Hello world!',
+    ingredient: async({name})=>{
+        return INGREDIENTS.get(name)
+    },
     newUser: async ({name, email, id}) =>{
         USERS.set(id, new User({name, email, id}))
         return USERS.get(id)
     },
+    newIngredient: async({name})=>{
+        INGREDIENTS.set(name, new Ingredient({name}))
+        return INGREDIENTS.get(name)
+    },
+    newRecipeIngredient: ({recipe, ingredient, amount, unit})=>{
+        var new_recipe_ingredient = new RecipeIngredient({recipe, ingredient, amount, unit})
+        if(INGREDIENTS.get(ingredient.name) == null){
+            INGREDIENTS.set(ingredient.name, new Ingredient({name:ingredient.name}))
+        }
+        RECIPEINGREDIENTS.set(recipe+ingredient, new_recipe_ingredient)
+        return RECIPEINGREDIENTS.get(recipe+ingredient)
+    },
+    newRecipe: async({id, name, ingredients})=>{
+        RECIPES.set(id, new Recipe({id:id, name:name, ingredients:ingredients.map((ingredient)=>{
+            return rootValue.newRecipeIngredient({recipe:name, ingredient:{name:ingredient.ingredient.name}, amount:ingredient.amount, unit:ingredient.unit})
+        })}))
+        console.log(JSON.stringify(RECIPES.get(id)))
+        return RECIPES.get(id)
+    },
     getUsers: () => USERS.values(),
     getCrops: () => CROPS.values(),
+    getIngredients: () => INGREDIENTS.values(),
+    getRecipes: () => RECIPES.values(),
+    getRecipeIngredients: () => RECIPEINGREDIENTS.values(),
     findCrop: ({filter}) => {
         return new Map(
             [...CROPS].filter(([k, {name}]) => name.includes(filter)
